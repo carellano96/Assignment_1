f2064662bf64204d1211e7b92f3b4849052c0367
carellano96 <carlos@172-17-17-72.dynapool.nyu.edu>
2018-02-05 14:41:10 -0500

    Initial Commit

---
+++ Assignment_1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Assignment_1
+//
+//  Created by carlos arellano on 2/5/18.
+//  Copyright © 2018 nyu.edu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
